a9cf45fcc942feba7e5b3968c97cbd1946ddb737
Henrik Albrechtsson <henrik.albrechtsson@gmail.com>
2013-12-16 10:19:03 +0100

    Update sql.php

--- sql.php
+++ sql.php
@@ -2,9 +2,14 @@
 
 include('sql.php');
 
+// SELECT
 $result = mysqli_query($con,'SELECT row FROM table WHERE row2 != value');
 while($row = mysqli_fetch_array($result)) {
   echo $row['row'];
 }
+
+// INSERT
+$query = 'INSERT INTO table (row, row2, row3) VALUES ("' . $row . '", "' . $row2 . '", "' . $row3 . '")';
+mysqli_query($con, $query);
 
 ?>
