dcacbd904e185de7e4806ab01ceb4d7477310faf
Henrik Albrechtsson <henrik.albrechtsson@gmail.com>
2014-12-12 19:00:52 +0100

    Error reporting and file get contents

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+function file_get_contents($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+  return $data;
+}
+
+?>
